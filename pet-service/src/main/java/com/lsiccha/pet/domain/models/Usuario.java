f86b8319006a24e8eff324accf1a32ce0ea384ce
LSiccha <gabrielluis_2001@hotmail.com>
2022-11-29 19:08:04 -0500

    entidades

---
+++ pet-service/src/main/java/com/lsiccha/pet/domain/models/Usuario.java
@@ -0,0 +1,21 @@
+package com.lsiccha.pet.domain.models;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Data
+@Entity
+@Table(name = "usuario")
+public class Usuario {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "email")
+    private String email;
+
+    @Column(name = "password")
+    private String password;
+}
