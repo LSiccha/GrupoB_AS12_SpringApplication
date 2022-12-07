688c48370448ca1bb26c2771fb168218c70c8e0c
LSiccha <gabrielluis_2001@hotmail.com>
2022-12-07 14:48:39 -0500

    more data xd

--- pet-service/src/main/resources/data.sql
+++ pet-service/src/main/resources/data.sql
@@ -6,8 +6,8 @@ INSERT INTO usuario (id, email, password) VALUES (3, 'adoptante02@gmail.com', 'a
 
 INSERT INTO admin (id, dni, nombres, usuario_id) VALUES (1, '7541234', 'Manuel Quispe', 1);
 
-INSERT INTO adoptante (id, fecha_registro, usuario_id) VALUES (1, '14/05/2022', 2);
-INSERT INTO adoptante (id, fecha_registro, usuario_id) VALUES (2, '14/06/2022', 3);
+INSERT INTO adoptante (id, fecha_registro, usuario_id) VALUES (1, '2022-05-14', 2);
+INSERT INTO adoptante (id, fecha_registro, usuario_id) VALUES (2, '2022-06-14', 3);
 
 INSERT INTO
     perfil_adoptante (id, celular, correo, direccion, dni, edad, nombres, adoptante_id)
@@ -18,10 +18,10 @@ VALUES
 INSERT INTO
     voluntario (id, celular, correo, direccion, dni, edad, fecha_registro, nombres, admin_id)
 VALUES
-    (1, '987456232', 'voluntario01@gmail.com', 'Su otra casa', '789456123', 24, '21/10/2022', 'Voluntario Desrosiers', 1),
-    (2, '789451232', 'voluntario02@gmail.com', 'Casa su otra', '789421323', 30, '14/10/2022', 'Voluntario Quispe', 1),
-    (3, '584657887', 'voluntario03@gmail.com', 'Su depa', '21312312', 18, '15/10/2022', 'Voluntario Trujillo', 1),
-    (4, '474855645', 'voluntario04@gmail.com', 'Su otro depa', '782131243', 16, '20/10/2022', 'Voluntario Grados', 1);
+    (1, '987456232', 'voluntario01@gmail.com', 'Su otra casa', '789456123', 24, '2022-10-21', 'Voluntario Desrosiers', 1),
+    (2, '789451232', 'voluntario02@gmail.com', 'Casa su otra', '789421323', 30, '2022-10-14', 'Voluntario Quispe', 1),
+    (3, '584657887', 'voluntario03@gmail.com', 'Su depa', '21312312', 18, '2022-10-15', 'Voluntario Trujillo', 1),
+    (4, '474855645', 'voluntario04@gmail.com', 'Su otro depa', '782131243', 16, '2022-10-20', 'Voluntario Grados', 1);
 
 INSERT INTO mascota (id, adoptada, nombres, raza, tamanio)
 VALUES
@@ -35,9 +35,9 @@ VALUES
 INSERT INTO
     solicitud (id, aceptada, en_espera, fecha_solicitud, mensaje, adoptante_id, mascota_id)
 VALUES
-    (1, false, true, '12/11/2022', 'Holaa :D', 1, 1),
-    (2, false, true, '14/11/2022', 'Holaa :D', 1, 2),
-    (3, false, false, '15/11/2022', 'Holaa :D', 1, 3),
-    (4, false, false, '16/11/2022', 'Holaa :D', 1, 6),
-    (5, true, false, '17/11/2022', 'Holaa :D', 2, 4),
-    (5, true, false, '18/11/2022', 'Holaa :D', 2, 5);
+    (1, false, true, '2022-11-12', 'Una solicitud en espera', 1, 1),
+    (2, false, true, '2022-11-14', 'Otra solicitud en espera', 1, 2),
+    (3, false, false, '2022-11-15', 'Una solicitud denegada', 1, 3),
+    (4, false, false, '2022-11-16', 'Otra solicitud denegada', 1, 6),
+    (5, true, false, '2022-11-17', 'Una solicitud aceptada', 2, 4),
+    (6, true, false, '2022-11-18', 'Otra solicitud aceptada', 2, 5);
